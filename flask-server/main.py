4cdb427f104e730a44d325bcbdb321b170bb5468
SagarNRao <sagarnrao154@gmail.com>
2025-02-26 18:14:51 +0530

    test commit

--- flask-server/main.py
+++ flask-server/main.py
@@ -18,6 +18,60 @@ startingMessageID = None
 endingMessageID = None
 messages = []
 
+url = "https://api.notion.com/v1/pages"
+
+NOTION_API_KEY = os.getenv("NOTION_API_KEY")
+DATABASE_ID = os.getenv("NOTION_DATABASE_ID")
+
+headers = {
+    "Authorization": f"Bearer {NOTION_API_KEY}",
+    "Content-Type": "application/json",
+    "Notion-Version": "2022-06-28",
+}
+
+
+NOTION_TOKEN = os.getenv("NOTION_API_KEY")
+DATABASE_ID = os.getenv("NOTION_DATABASE_ID")
+headers = {
+    "Authorization": "Bearer " + NOTION_TOKEN,
+    "Content-Type": "application/json",
+    # Check what is the latest version here: https://developers.notion.com/reference/changes-by-version
+    "Notion-Version": "2022-06-28",
+}
+
+
+def makePage(data: dict):
+    create_url = "https://api.notion.com/v1/pages"
+    payload = {"parent": {"database_id": DATABASE_ID}, "properties": data}
+    res = requests.post(create_url, headers=headers, json=payload)
+    if res.status_code == 200:
+        print(f"{res.status_code}: Page created successfully")
+    else:
+        print(f"{res.status_code}: Error during page creation")
+    return res
+
+
+properties = {
+    "Name": {
+        "title": [
+            {
+                "text": {
+                    "content": "Discord Channel Export"
+                }
+            }
+        ]
+    },
+    "Content": {
+        "rich_text": [
+            {
+                "text": {
+                    "content": "somebody once told me the wooooorld is gonna roll me i aint hte sharpest tool in the sheeeeed"
+                }
+            }
+        ]
+    }
+}
+
 
 @client.event
 async def on_ready():
@@ -26,7 +80,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    global startingMessageID, endingMessageID, messages
+    global startingMessageID, endingMessageID, messages, properties
 
     if message.author.bot:
         return  # Ignore bot messages
@@ -55,6 +109,7 @@ async def on_message(message):
                         for attachment in msg.attachments:
                             if attachment.content_type.startswith('image/'):
                                 content += f'\nImage: {attachment.url}'
+                    makePage(content)
                     await message.channel.send(content)
             except discord.NotFound:
                 await message.channel.send("Could not find one of the messages. Please check the IDs.")
